1d1ac37ecf59ad20cc32884b9c7fd1c25c7c5f31
Carly McVay <carlyjanemcvay@gmail.com>
2021-04-02 14:55:19 -0700

    fixed images not loading

--- src/pages/Resume.js
+++ src/pages/Resume.js
@@ -5,7 +5,7 @@ export default function Resume() {
         <div className="row d-flex justify-content-center mt-5">
             <div>
                 <a href="./resume.pdf" download>
-                    <img src="./images/resumeimg.png" alt="resume" width="100%" height="100%"/>
+                    <img src="https://user-images.githubusercontent.com/70187365/113456892-47f3d500-93c3-11eb-92b0-cf42cdebb214.png" alt="resume" width="100%" height="100%"/>
                     <br />
                     click to download
                 </a>
